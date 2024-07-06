2179f77feca1ab49c34865ff83146caa1b2c8848
JAM <99.1.some@gmail.com>
2024-07-06 14:30:10 +0900

    Style: ESLint, Prettier 설정 파일 추가
    ESLint, Prettier 설정 파일 추가
    
    Resolves #2

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+   pluginJs.configs.recommended,
+   {
+      languageOptions: {
+         globals: {
+            ...globals.browser,
+            ...globals.node,
+         },
+      },
+      rules: {
+         'no-unused-vars': 'warn',
+      },
+   },
+];
