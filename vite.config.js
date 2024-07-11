a8eed442cba21dc2c716b8233eb9d337478a42c5
JAM <99.1.some@gmail.com>
2024-07-11 14:14:34 +0900

    Modify : 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
          input: {
             main: resolve(__dirname, 'index.html'),
             homepage: resolve(__dirname, './src/pages/homepage/homepage.html'),
+            mypagemain: resolve(__dirname, '/src/pages/my-page/main/main.html'),
+            reviewdate : resolve(__dirname, '/src/pages/review-create/date-place.html')
          },
       },
    },
