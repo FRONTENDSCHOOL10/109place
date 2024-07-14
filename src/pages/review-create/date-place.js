f8e13d2e1d4ba05096972d99a43d4184192a382f
minnje <167839981+minnje@users.noreply.github.com>
2024-07-15 00:23:15 +0900

    Feat: date-place.js 달력

--- src/pages/review-create/date-place.js
+++ src/pages/review-create/date-place.js
@@ -1,1 +1,24 @@
 import '/src/pages/review-create/date-place.scss';
+
+const goBack = document.querySelector('.go-back');
+const exit = document.querySelector('.right-exit');
+const datePicker = document.querySelector('.date-picker');
+const dateInput = document.querySelector('.date-input--off');
+
+const handleGoBack = () => {
+   location.href = '/src/pages/search-detail/search-detail.html';
+};
+
+const handleExit = () => {
+   location.href = '/src/pages/homepage/homepage.html';
+};
+
+const handleDatePicker = () => {
+   dateInput.classList.add('date-input--on');
+   datePicker.classList.add('date-label--off');
+   datePicker.innerText = '';
+};
+
+goBack.addEventListener('click', handleGoBack);
+exit.addEventListener('click', handleExit);
+datePicker.addEventListener('click', handleDatePicker);
