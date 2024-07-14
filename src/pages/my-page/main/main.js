fe19cfc15ba64911e4d11836d1d2afb7bc99c45a
박새롬 <qkrtofhaaa@gmail.com>
2024-07-15 00:24:31 +0900

    Feat: 마이페이지 메인 스크롤기능 구현,리팩토링 해야함

--- src/pages/my-page/main/main.js
+++ src/pages/my-page/main/main.js
@@ -1,1 +1,37 @@
-// 폴더사라짐방지
+const myPageSection = document.querySelector('.my-page__section');
+const myPageHeader = document.querySelector('.my-page__header');
+const myPageFooter = document.querySelector('.my-page__footer');
+const reviewContainer = document.querySelector('.review--container');
+const footerButtonTop = document.querySelector('.footer--button-top');
+
+const vwToPx = window.innerWidth;
+const remToPx =
+   parseFloat(getComputedStyle(document.documentElement).fontSize) * 12.4375;
+const scrollHandler = () => {
+   const vwToPx = window.innerWidth;
+   const remToPx =
+      parseFloat(getComputedStyle(document.documentElement).fontSize) * 12.4375;
+
+   if (reviewContainer.scrollTop > 0) {
+      myPageHeader.classList.add('hidden');
+      myPageFooter.classList.add('hidden');
+      reviewContainer.style.height = vwToPx + remToPx + 'px';
+      footerButtonTop.classList.remove('hidden');
+   } else {
+      myPageHeader.classList.remove('hidden');
+      reviewContainer.style.height = vwToPx - remToPx + 'px';
+      myPageFooter.classList.remove('hidden');
+      footerButtonTop.classList.add('hidden');
+   }
+};
+
+const clickTopMoveHandler = () => {
+   reviewContainer.scrollTo({
+      top: 1,
+      behavior: 'smooth',
+   });
+};
+
+reviewContainer.addEventListener('scroll', scrollHandler);
+
+footerButtonTop.addEventListener('click', clickTopMoveHandler);
