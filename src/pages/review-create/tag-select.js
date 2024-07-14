bbd89848d54fcf011250bbadd610ce0f4c58aa6c
minnje <167839981+minnje@users.noreply.github.com>
2024-07-15 01:27:10 +0900

    Modify: 리뷰 태그 선택 페이지 코드 수정

--- src/pages/review-create/tag-select.js
+++ src/pages/review-create/tag-select.js
@@ -1,1 +1,16 @@
 import '/src/pages/review-create/tag-select.scss';
+
+import { getNode } from 'kind-tiger';
+
+const exit = getNode('.right-exit');
+const btnNext = getNode('.btn__next');
+
+const handleExit = () => {
+   location.href = '/src/pages/homepage/homepage.html';
+};
+const handleBtnNext = () => {
+   location.href = '/src/pages/review-create/img-content.html';
+};
+
+exit.addEventListener('click', handleExit);
+btnNext.addEventListener('click', handleBtnNext);
