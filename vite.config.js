4c68eae0a56d225259bf2874e58ee526dc1a88ed
JAM <99.1.some@gmail.com>
2024-07-15 12:04:40 +0900

    Modify : vte.config.js 에 각 페이지 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,40 @@ export default defineConfig({
             main: resolve(__dirname, 'index.html'),
             homepage: resolve(__dirname, './src/pages/homepage/homepage.html'),
             mypagemain: resolve(__dirname, '/src/pages/my-page/main/main.html'),
-            reviewdate : resolve(__dirname, '/src/pages/review-create/date-place.html'),
-            sample : resolve(__dirname, '/src/pages/sample/sample.html')
+            reviewdate: resolve(
+               __dirname,
+               '/src/pages/review-create/date-place.html'
+            ),
+            login: resolve(__dirname, '/src/pages/login/login.html'),
+            signup: resolve(__dirname, '/src/pages/signup/signup.html'),
+            placedetail: resolve(
+               __dirname,
+               '/src/pages/place-detail/place-detail.html'
+            ),
+            reviewimg: resolve(
+               __dirname,
+               '/src/pages/review-create/img-content.html'
+            ),
+            reviewtag: resolve(
+               __dirname,
+               '/src/pages/review-create/tag-select.html'
+            ),
+            reviewdone: resolve(
+               __dirname,
+               '/src/pages/review-done/review-done.html'
+            ),
+            reviewsearch: resolve(
+               __dirname,
+               '/src/pages/review-search/review-search.html'
+            ),
+            svaedplace: resolve(
+               __dirname,
+               '/src/pages/saved-places/saved-places.html'
+            ),
+            searchdetail: resolve(
+               __dirname,
+               '/src/pages/search-detail/search-detail.html'
+            ),
          },
       },
    },
