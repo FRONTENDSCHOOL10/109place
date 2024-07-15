99a624dd82eca932ee74fc683745a5d12f4bddec
minnje <167839981+minnje@users.noreply.github.com>
2024-07-15 16:41:19 +0900

    Feat: 체크박스 두 개까지 제한 기능

--- src/pages/review-create/tag-select.js
+++ src/pages/review-create/tag-select.js
@@ -1,6 +1,24 @@
 import '/src/pages/review-create/tag-select.scss';
-
 import { getNode } from 'kind-tiger';
+
+//체크박스 2개 제한
+const limitedCheckbox = (cls) => {
+   let checkboxes = document.querySelectorAll(`.${cls}`);
+
+   checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('change', () => {
+         let checkedCount = Array.from(checkboxes).filter(
+            (i) => i.checked
+         ).length;
+
+         if (checkedCount > 2) {
+            this.checked = false;
+         }
+      });
+   });
+};
+limitedCheckbox('limited-checkbox');
+limitedCheckbox('limited-checkbox2');
 
 const exit = getNode('.right-exit');
 const btnNext = getNode('.btn__next');
