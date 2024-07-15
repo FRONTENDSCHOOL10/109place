9b28975b35539320ee11259b01903764d25bc1cd
박새롬 <qkrtofhaaa@gmail.com>
2024-07-15 14:13:22 +0900

    HTML:CSS .env and package.json

--- src/pages/my-page/main/main.js
+++ src/pages/my-page/main/main.js
@@ -4,22 +4,18 @@ const myPageFooter = document.querySelector('.my-page__footer');
 const reviewContainer = document.querySelector('.review--container');
 const footerButtonTop = document.querySelector('.footer--button-top');
 
-const vwToPx = window.innerWidth;
+const vhToPx = window.innerHeight;
 const remToPx =
    parseFloat(getComputedStyle(document.documentElement).fontSize) * 12.4375;
 const scrollHandler = () => {
-   const vwToPx = window.innerWidth;
-   const remToPx =
-      parseFloat(getComputedStyle(document.documentElement).fontSize) * 12.4375;
-
    if (reviewContainer.scrollTop > 0) {
       myPageHeader.classList.add('hidden');
       myPageFooter.classList.add('hidden');
-      reviewContainer.style.height = vwToPx + remToPx + 'px';
+      reviewContainer.style.height = vhToPx + remToPx + 'px';
       footerButtonTop.classList.remove('hidden');
    } else {
       myPageHeader.classList.remove('hidden');
-      reviewContainer.style.height = vwToPx - remToPx + 'px';
+      reviewContainer.style.height = vhToPx - remToPx + 'px';
       myPageFooter.classList.remove('hidden');
       footerButtonTop.classList.add('hidden');
    }
