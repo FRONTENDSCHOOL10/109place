1c41e5e2d3d53c1145cb3660b7c98f8160ec3017
박새롬 <qkrtofhaaa@gmail.com>
2024-07-15 17:04:30 +0900

    feat : db 연결 시도

--- src/pages/my-page/main/main.js
+++ src/pages/my-page/main/main.js
@@ -1,24 +1,21 @@
-import PocketBase from 'pocketbase';
+// import PocketBase from 'pocketbase';
 
-const pb = new PocketBase('https://vanilla-109place.pockethost.io');
+// const pb = new PocketBase('https://vanilla-109place.pockethost.io');
 
-...
+// ...
 
-// fetch a paginated records list
-const resultList = await pb.collection('review').getList(1, 50, {
-    filter: 'created >= "2022-01-01 00:00:00" && someField1 != someField2',
-});
+// // fetch a paginated records list
+// const resultList = await pb.collection('review').getList(1, 50, {
+//     filter: 'created >= "2022-01-01 00:00:00" && someField1 != someField2',
+// });
 
-
-console.log(resultList)
+// console.log(resultList);
 
 const myPageSection = document.querySelector('.my-page__section');
 const myPageHeader = document.querySelector('.my-page__header');
 const myPageFooter = document.querySelector('.my-page__footer');
 const reviewContainer = document.querySelector('.review--container');
 const footerButtonTop = document.querySelector('.footer--button-top');
-
-
 
 const scrollHandler = () => {
    if (reviewContainer.scrollTop > 0) {
