2a7416f68397dbff12e08835629ffc32e75144c8
박새롬 <qkrtofhaaa@gmail.com>
2024-07-15 14:22:56 +0900

    HTML:CSS .env and package.json 파잃삭제

--- src/pages/my-page/main/main.js
+++ src/pages/my-page/main/main.js
@@ -4,18 +4,18 @@ const myPageFooter = document.querySelector('.my-page__footer');
 const reviewContainer = document.querySelector('.review--container');
 const footerButtonTop = document.querySelector('.footer--button-top');
 
-const vhToPx = window.innerHeight;
 const remToPx =
    parseFloat(getComputedStyle(document.documentElement).fontSize) * 12.4375;
+
 const scrollHandler = () => {
    if (reviewContainer.scrollTop > 0) {
       myPageHeader.classList.add('hidden');
       myPageFooter.classList.add('hidden');
-      reviewContainer.style.height = vhToPx + remToPx + 'px';
+      reviewContainer.style.height = `${vhToPx + remToPx}px`;
       footerButtonTop.classList.remove('hidden');
    } else {
       myPageHeader.classList.remove('hidden');
-      reviewContainer.style.height = vhToPx - remToPx + 'px';
+      reviewContainer.style.height = `${vhToPx - remToPx}px`;
       myPageFooter.classList.remove('hidden');
       footerButtonTop.classList.add('hidden');
    }
