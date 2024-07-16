20b0b40c19d076931adcae0b5adc1093c127ce88
2yerin <lyerin0113@naver.com>
2024-07-16 09:59:39 +0900

    DUpdate : 브랜치 이동을 위한 커밋

--- src/pages/place-detail/place-detail.js
+++ src/pages/place-detail/place-detail.js
@@ -1,5 +1,6 @@
 import '/src/pages/place-detail/place-detail.scss';
 import pb from '/src/lib/utils/pocketbase';
+import { getStorage, setStorage } from "kind-tiger";
 
 // 리뷰 이미지 스와이퍼
 var swiper = new Swiper(".swiper", {
@@ -8,24 +9,73 @@ var swiper = new Swiper(".swiper", {
 });
 
 
-async function storeRender(){
 
-  const storeData = await pb
-  .collection('stores')
-  .getFullList({
-    sort: '-created'
-  });
+// 검색 페이지에서 검색한 가게 정보가 DB에 존재하는지 확인하는 함수
+async function matchLocalWithDB(){
 
-  console.log(storeData);
+  // 검색 페이지에서 저장한 가게 이름과 주소 받아오기 (로컬 스토리지)
+  const searchData = {
+    name: '족발땡겨', //추후에 검색 페이지에서 로컬 스토리지에 저장한 데이터 받아오기
+    category: '고기',
+    address: '서울시 153-23',
+    phone_number: 12334454,
+    business_hours: '8:00~21:00'
+  };
+
+  // 포켓 베이스 접근
+  const storeRecordData = await pb
+    .collection('stores')
+    .getFullList();
+
+
+  const foundData = storeRecordData.filter( data => data.name === searchData.name && data.address === searchData.address);
+
+  if(!foundData.length){
+    insertData(searchData)
+  }
+
+  return foundData;
 }
 
-storeRender();
+
+
+// stores 테이블에 Data 저장하는 함수
+async function insertData(searchData){
+
+  console.log(searchData);
+  // const name = 
+
+  // const data={
+  //   name = name,
+  //   category:,
+  //   address:p,
+  //   phone_number:p,
+  //   reviews_count:,
+  //   business_hours:,
+  //   images:,
+  // };
+
+  // await pb
+  //   .collection('stores')
+  //   .create(data);
+}
+
+
+
+
+
+
+// 전체 로직 실행
+matchLocalWithDB();
+
 
 
 
 // 검색 -> 장소 상세
 // 1. 검색에서 받은 데이터가 stores 테이블 안에 있는지 체크
-//  1) 검색해서 장소 상세로 페이지 이동 시 클릭한 정보의 가게 이름과 주소 받아오기
+//  1) 검색해서 장소 상세로 페이지 이동 시 클릭한 정보의 가게 이름과 주소 받아오기(로컬 스토리지)
+//    sample) 로컬 스토리지에 가게 이름과 주소 저장. (페이지 연결 시 추후 삭제)
+//            로컬 스토리지에서 가게 이름, 주소 가져오기.
 //  2) 받아온 가게 이름과 주소가 모두 동일한 데이터가 테이블에 존재하는지 확인
 
 // 2. 데이터 존재 여부에 따라 해당 방법으로 렌더링
