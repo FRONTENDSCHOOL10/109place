a3f06140bfc19b3d6df1d0d55bef24a9ec9de1b3
박새롬 <qkrtofhaaa@gmail.com>
2024-07-16 12:25:14 +0900

    feat : 리뷰디테일 페이지 이미지 연동

--- src/pages/my-page/review/review.js
+++ src/pages/my-page/review/review.js
@@ -11,36 +11,46 @@ import pb from '/src/lib/utils/pocketbase';
 
    const stores = await pb.collection('stores').getOne(selectReview.stores_id);
 
-   console.log(stores);
+   const BASE_URL = 'https://vanilla-109place.pockethost.io';
+
+   const reviewImageUrl = `${BASE_URL}/api/files/review/${selectReview.id}/${selectReview.image[0]}`;
+
+   const reviewDetailFicture = document.querySelector(
+      '.review__detail--ficture'
+   );
 
    const userName = document.querySelector('.profile__user-name');
-   userName.textContent = record.username;
-
    const storeName = document.querySelector('.review-detail__store-name');
-   storeName.textContent = stores.name;
-
-   const storeCategory = document.querySelector(
-      '.review-detail__store-info--name'
-   );
-   storeCategory.textContent = stores.category;
 
    const storeAddress = document.querySelector(
       '.review-detail__store-info--address'
    );
+
+   const storeCategory = document.querySelector(
+      '.review-detail__store-info--name'
+   );
+   const reviewHow = document.querySelector('.review-info--how');
+   const waitingTime = document.querySelector('.review-info--delay');
+   const reviewWith = document.querySelector('.review-info--with');
+
+   const reviewContent = document.querySelector('.review-detail__content');
+
+   reviewDetailFicture.style.backgroundImage = `url(${reviewImageUrl})`;
+
+   userName.textContent = record.username;
+
+   storeName.textContent = stores.name;
+   storeCategory.textContent = stores.category;
    storeAddress.textContent = stores.address;
 
-   const reviewHow = document.querySelector('.review-info--how');
    reviewHow.textContent = selectReview.how;
 
-   const waitingTime = document.querySelector('.review-info--delay');
    waitingTime.textContent = selectReview.delay;
 
-   const reviewWith = document.querySelector('.review-info--with');
    reviewWith.textContent =
       selectReview.with.length === 1
          ? selectReview.with[0]
          : `${selectReview.with[0]} / ${selectReview.with[1]}`;
 
-   const reviewContent = document.querySelector('.review-detail__content');
    reviewContent.textContent = selectReview.review;
 })();
