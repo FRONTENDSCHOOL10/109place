1572a73dad722680101fe05423f511a3e4eac84a
박새롬 <qkrtofhaaa@gmail.com>
2024-07-16 11:00:25 +0900

    feat : store db정보 삽입

--- src/pages/my-page/review/review.js
+++ src/pages/my-page/review/review.js
@@ -1,32 +1,46 @@
 import pb from '/src/lib/utils/pocketbase';
 
-const record = await pb.collection('users').getOne(pb.authStore.model.id);
+(async () => {
+   const record = await pb.collection('users').getOne(pb.authStore.model.id);
 
-const urlParams = new URLSearchParams(window.location.search);
+   const urlParams = new URLSearchParams(window.location.search);
 
-const selectReview = await pb
-   .collection('review')
-   .getOne(urlParams.get('reviewId'));
+   const selectReview = await pb
+      .collection('review')
+      .getOne(urlParams.get('reviewId'));
 
-console.log(selectReview);
+   const stores = await pb.collection('stores').getOne(selectReview.stores_id);
 
-const userName = document.querySelector('.profile__user-name');
-userName.textContent = record.username;
+   console.log(stores);
 
-const storeName = document.querySelector('.review-detail__store-name');
-storeName.textContent = selectReview.stores_id;
+   const userName = document.querySelector('.profile__user-name');
+   userName.textContent = record.username;
 
-const reviewHow = document.querySelector('.review-info--how');
-reviewHow.textContent = selectReview.how;
+   const storeName = document.querySelector('.review-detail__store-name');
+   storeName.textContent = stores.name;
 
-const waitingTime = document.querySelector('.review-info--delay');
-waitingTime.textContent = selectReview.delay;
+   const storeCategory = document.querySelector(
+      '.review-detail__store-info--name'
+   );
+   storeCategory.textContent = stores.category;
 
-const reviewWith = document.querySelector('.review-info--with');
-reviewWith.textContent =
-   selectReview.with.length === 1
-      ? selectReview.with[0]
-      : `${selectReview.with[0]} / ${selectReview.with[1]}`;
+   const storeAddress = document.querySelector(
+      '.review-detail__store-info--address'
+   );
+   storeAddress.textContent = stores.address;
 
-const reviewContent = document.querySelector('.review-detail__content');
-reviewContent.textContent = selectReview.review;
+   const reviewHow = document.querySelector('.review-info--how');
+   reviewHow.textContent = selectReview.how;
+
+   const waitingTime = document.querySelector('.review-info--delay');
+   waitingTime.textContent = selectReview.delay;
+
+   const reviewWith = document.querySelector('.review-info--with');
+   reviewWith.textContent =
+      selectReview.with.length === 1
+         ? selectReview.with[0]
+         : `${selectReview.with[0]} / ${selectReview.with[1]}`;
+
+   const reviewContent = document.querySelector('.review-detail__content');
+   reviewContent.textContent = selectReview.review;
+})();
