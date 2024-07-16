932263b9bf6843bd69bd8af66fa105f125a82735
박새롬 <qkrtofhaaa@gmail.com>
2024-07-16 15:10:54 +0900

    HTML&CSS : 마이페이지 프로필 마크업

--- src/pages/my-page/profile/profile.js
+++ src/pages/my-page/profile/profile.js
@@ -1,1 +1,33 @@
-// 폴더사라짐방지
+import pb from '/src/lib/utils/pocketbase';
+
+(async () => {
+   const reviewList = await pb.collection('review').getList(1, 50);
+   const user = await pb.collection('users').getOne(pb.authStore.model.id);
+   const storeList = await pb.collection('stores').getList(1, 50);
+
+   const BASE_URL = 'https://vanilla-109place.pockethost.io';
+
+   const myPageProfileImg = document.querySelector(
+      '.my-page__profile__section--img'
+   );
+
+   const profileImageUrl = `${BASE_URL}/api/files/users/${user.id}/${user.profile_picture}`;
+
+   function handleCount() {
+      let textLength = this.value.length;
+
+      if (textLength > 400) {
+         this.value = this.value.substring(0, 400);
+         textLength = 400;
+      }
+      textCount.textContent = `${textLength}`;
+   }
+
+   const textArea = document.querySelector('.input-count__input');
+   const textCount = document.querySelector('.input-count__count');
+   const textUserName = document.querySelector('.user-name__input');
+   textArea.addEventListener('input', handleCount);
+
+   myPageProfileImg.style.backgroundImage = `url(${profileImageUrl})`;
+   textUserName.textContent = user.username;
+})();
