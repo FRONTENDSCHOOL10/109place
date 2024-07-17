5c233a5193ba33459ab8c5c5b0d0516fc52051da
JAM <99.1.some@gmail.com>
2024-07-17 16:14:10 +0900

    HtmlCss : 리뷰 끝 페이지 마크업, 기능 구현중

--- src/pages/review-done/review-done.js
+++ src/pages/review-done/review-done.js
@@ -1,1 +1,18 @@
 import '/src/pages/review-done/review-done.scss';
+import pb from '/src/lib/utils/pocketbase';
+
+let placeName = document.querySelector('.done__comment__text__place');
+
+// 지금 로그인한 유저 정보
+// 제일 최근 리뷰 가져오기
+const userInfo = await pb.collection('users').getOne(pb.authStore.model.id);
+const userId = userInfo.id;
+
+console.log(userId);
+
+const selectReview = await pb.collection('review').getList(1, 1,{
+  sort: '-created',
+  filter: `users_id="${userId}"`,
+});
+
+console.log(selectReview.items[0].stores_id);
