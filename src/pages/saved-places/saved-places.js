419918c8dcb05da4ea8b3bdaf918a46142a32ab6
2yerin <lyerin0113@naver.com>
2024-07-18 04:29:51 +0900

    Modify: : 장소 저장 페이지(저장 기능 중간 커밋)

--- src/pages/saved-places/saved-places.js
+++ src/pages/saved-places/saved-places.js
@@ -1,3 +1,51 @@
 import '/src/pages/saved-places/saved-places.scss';
 import '/src/layout/footer/footer.js';
 import pb from '/src/lib/utils/pocketbase';
+import { getPbImageURL , deleteStorage, getStorage, insertBefore, insertAfter, insertFirst, insertLast } from "kind-tiger";
+
+async function renderSavePlace(){
+  const userInfo = await getStorage('pocketbase_auth');
+  const userId = userInfo.model.id;
+  const savePlace = await pb
+    .collection('bookmark')
+    .getList(1, 50, {
+      filter: `user_id = "${userId}"`,
+    });
+  let placeInfo;
+  const BASE_URL = 'https://vanilla-109place.pockethost.io';
+
+
+  const saveCountTemplate =`
+    <span>${savePlace.items.length}</span>
+  `
+  
+  insertAfter('.header > h1',saveCountTemplate);
+
+
+
+  for(let item of savePlace.items){
+    placeInfo = await pb.collection('stores').getOne(item.stores_id);
+
+    const savePlaceTemplate =`
+      <article class="saved-place">
+        <div class="place-text">
+          <div class="place-text__title">
+            <h2>${placeInfo.name}</h2>
+            <span>${placeInfo.category}</span>
+          </div>
+
+          <p>${placeInfo.address}</p>
+        </div>
+
+        <div class="place-img">
+          <img src="${BASE_URL}/api/files/${placeInfo.collectionId}/${placeInfo.id}/${imgs[0]}" alt="" class="place-img--left"/>
+          <img src="${BASE_URL}/api/files/${placeInfo.collectionId}/${placeInfo.id}/${imgs[0]}" alt="" class="place-img--right"/>
+        </div>
+      </article>
+    `
+
+    insertAfter('.saved-places',savePlaceTemplate);
+  }
+}
+
+renderSavePlace();
