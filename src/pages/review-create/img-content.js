023616e4e3e3c4004e12f3bb3e114ba075420f07
minnje <167839981+minnje@users.noreply.github.com>
2024-07-17 22:05:36 +0900

    Modify:중간머지

--- src/pages/review-create/img-content.js
+++ src/pages/review-create/img-content.js
@@ -1,28 +1,25 @@
 import '/src/pages/review-create/img-content.scss';
+
 import pb from '/src/lib/utils/pocketbase';
 
-const image = new FormData();
+console.log(pb);
+
+const formData = new FormData();
+
+const fileInput = document.querySelector('.upload-button');
+fileInput.addEventListener('change', function () {
+   for (let file of fileInput.files) {
+      console.log('file', file);
+      const fd = formData.append('image', file);
+      console.log('fd', fd);
+   }
+});
+pb.collection('users').create(formData);
 
 const textarea = document.querySelector('textarea');
-const fileInput = document.querySelector('.upload-button');
 
 const handleReview = (e) => {
-   console.log(e.target.value);
    localStorage.setItem('review', e.target.value);
 };
 
-fileInput.addEventListener('change', function () {
-   for (let file of fileInput.files) {
-      image.append('image', file);
-   }
-});
-
-image.append('title', 'Hello world!');
-
-// // set some other regular text field value
-// formData.append('title', 'Hello world!');
-
-await pb.collection('review').create(image);
-
 textarea.addEventListener('input', handleReview);
-// imageUpload.addEventListener('change', handleImg);
