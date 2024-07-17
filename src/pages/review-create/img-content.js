5450bcfa3eaa4131e63976a64ed30f345a64ca65
minnje <167839981+minnje@users.noreply.github.com>
2024-07-17 17:22:37 +0900

    Modify: 사진추가 리뷰작성 페이지 코드 수정

--- src/pages/review-create/img-content.js
+++ src/pages/review-create/img-content.js
@@ -1,1 +1,28 @@
 import '/src/pages/review-create/img-content.scss';
+import pb from '/src/lib/utils/pocketbase';
+
+const image = new FormData();
+
+const textarea = document.querySelector('textarea');
+const fileInput = document.querySelector('.upload-button');
+
+const handleReview = (e) => {
+   console.log(e.target.value);
+   localStorage.setItem('review', e.target.value);
+};
+
+fileInput.addEventListener('change', function () {
+   for (let file of fileInput.files) {
+      image.append('image', file);
+   }
+});
+
+image.append('title', 'Hello world!');
+
+// // set some other regular text field value
+// formData.append('title', 'Hello world!');
+
+await pb.collection('review').create(image);
+
+textarea.addEventListener('input', handleReview);
+// imageUpload.addEventListener('change', handleImg);
