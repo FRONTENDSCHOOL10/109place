958696e2f377cfb53231086623c33980e0de0cb1
minnje <167839981+minnje@users.noreply.github.com>
2024-07-17 15:02:17 +0900

    Modify: tag-select.html 헤더 링크 변경

--- src/pages/review-create/tag-select.js
+++ src/pages/review-create/tag-select.js
@@ -1,12 +1,9 @@
 import '/src/pages/review-create/tag-select.scss';
 import { getNode } from 'kind-tiger';
 
-const exit = getNode('.right-exit');
 const btnNext = getNode('.btn__next');
 const reviewTags = document.querySelectorAll('.review__tag--input');
-const handleExit = () => {
-   location.href = '/src/pages/homepage/homepage.html';
-};
+
 const handleBtnNext = () => {
    location.href = '/src/pages/review-create/img-content.html';
 };
@@ -15,7 +12,6 @@ const handleReviewTag = (e) => {
    localStorage.setItem(e.target.name, e.target.value);
 };
 
-exit.addEventListener('click', handleExit);
 btnNext.addEventListener('click', handleBtnNext);
 
 reviewTags.forEach((reviewTag) => {
