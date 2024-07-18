b97adb3dc60008cb67cf95205690072301a1a529
박새롬 <qkrtofhaaa@gmail.com>
2024-07-18 16:02:06 +0900

    feat : 프로필 수정, 소개 수정, 오류있음

--- src/pages/my-page/profile/profile.js
+++ src/pages/my-page/profile/profile.js
@@ -26,13 +26,34 @@ import pb from '/src/lib/utils/pocketbase';
    count.textContent = user.username.length;
    const textArea = document.querySelectorAll('.input-count__input');
    const textUserName = document.querySelector('.user-name__input');
+   const textUserInfo = document.querySelector('.user-info__input');
    textArea.forEach((item) => {
       item.addEventListener('input', handleCount);
    });
 
    myPageProfileImg.style.backgroundImage = `url(${profileImageUrl})`;
    textUserName.textContent = user.username;
+   textUserInfo.textContent = user.self_introduction;
+   const textAreaAll = document.querySelectorAll('textarea');
 
+   const userInfoChange = async () => {
+      const data = {
+         username: textAreaAll[0].value,
+         emailVisibility: user.emailVisibility,
+         password: user.password,
+         passwordConfirm: user.passwordConfirm,
+         oldPassword: user.oldPassword,
+         self_introduction: textAreaAll[1].value,
+         number_of_reviews: user.number_of_reviews,
+      };
+
+      const record = await pb.collection('users').update(user.id, data);
+      location.href = `/src/pages/my-page/main/main.html`;
+   };
+
+   const profileSubmitBtn = document.querySelector('.profile-submit');
+
+   profileSubmitBtn.addEventListener('click', userInfoChange);
    const headerBackBtn = document.querySelector('.profile__header--back-btn');
 
    const clickBackHandler = () => {
